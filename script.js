c0db90405ad086c06beacbaba20b9b21e932e752
Tetiana Voichak <tetiana.ozatska2012@gmail.com>
2025-03-24 13:56:59 +0100

    Refactor: add comments and improve button logic in JS

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-//адаптив меню
+//adaptive menu
 const menuBtn = document.querySelector(".menu-btn-mobile");
 const menuMobile = document.querySelector(".menu-mobile");
 const promoMobile = document.querySelector(".promo_inner");
-  
+
 menuBtn.addEventListener('click', () => {
   menuMobile.classList.toggle('menu--open');
   promoMobile.classList.toggle('menu-open');
+
 });
 
 class MyBlockClass {
@@ -14,22 +15,56 @@ class MyBlockClass {
     this.div = div;
     this.info = info;
   }
+  // a function to switch:
+  changeStyle = () => {
+    console.log('changeStyle called');
+    console.log(this.info);
+    const computedStyle = window.getComputedStyle(this.info);
+    const check = computedStyle.visibility == "hidden";
+    console.log(check); // Печать текущего состояния visibility
 
-  onButtonClick() {
-    if (this.info.style.visibility == "visible") {
-      this.btn.textContent = "+";
-      this.info.style.visibility = "hidden";
-      this.div.style.height = "71px";
-      this.div.style.background = "#fff";
-      this.info.style.background = "#fff";
-      this.div.style.color = "#333";
-    } else {
-      this.btn.textContent = "-";
-      this.info.style.visibility = "visible";
-      this.div.style.height = "102px";
-      this.div.style.background = "#F0F0F0";
-      this.info.style.background = "#F0F0F0";
+    // We create an object that has the properties as Name Value Pairs:
+    const infoEnabledStyle = {
+      visibility: "visible",
+      background: "#F0F0F0",
+      height: "102px",
     }
+    // Another object contains the other properties:
+    const infoDisabledStyle = {
+      visibility: "hidden",
+      background: "#fff",
+      height: "71px",
+      color: "#333",
+    }
+    //Change the button text depending on the state
+    this.btn.textContent = check ? "-" : "+";
+
+    // Apply styles via condition
+    if (check) {
+      for (const prop in infoEnabledStyle) {
+        this.info.style[prop] = infoEnabledStyle[prop]
+      }
+
+      // Apply styles to the div for the open state
+      this.div.style.height = infoEnabledStyle.height;
+      this.div.style.background = infoEnabledStyle.background;
+      this.div.style.color = "#333";
+    }
+    else {
+      for (const prop in infoDisabledStyle) {
+        this.info.style[prop] = infoDisabledStyle[prop]
+      }
+      // Apply styles to the div for the open state
+      this.div.style.height = infoDisabledStyle.height;
+      this.div.style.background = infoDisabledStyle.background;
+      this.div.style.color = "#333";
+    }
+
+  };
+  onButtonClick() {
+    console.log('onButtonClick called');
+
+    this.changeStyle();
   }
 }
 
@@ -37,72 +72,58 @@ let buttonElemServ = document.querySelector(".btn-info-charakt-serv");
 let divChange1 = document.querySelector(".punkt-serv");
 let outputMoreServ = document.querySelector(".more-info-serv");
 let bl1 = new MyBlockClass(buttonElemServ, divChange1, outputMoreServ);
-let newFunctServ = function () {
-  buttonElemServ.addEventListener("click", bl1.onButtonClick());
-};
+buttonElemServ.addEventListener("click", () => bl1.onButtonClick());
 
 let buttonElemPark = document.querySelector(".btn-info-charakt-park");
 let divChange2 = document.querySelector(".punkt-park");
 let outputMorePark = document.querySelector(".more-info-park");
 let bl2 = new MyBlockClass(buttonElemPark, divChange2, outputMorePark);
-let newFunctPark = function () {
-  buttonElemPark.addEventListener("click", bl2.onButtonClick());
-};
+buttonElemPark.addEventListener("click", () => bl2.onButtonClick());
 
 let buttonElemVideo = document.querySelector(".btn-info-charakt-video");
 let divChange3 = document.querySelector(".punkt-video");
 let outputMoreVideo = document.querySelector(".more-info-video");
 let bl3 = new MyBlockClass(buttonElemVideo, divChange3, outputMoreVideo);
-let newFunctVideo = function () {
-  buttonElemVideo.addEventListener("click", bl3.onButtonClick());
-};
+buttonElemVideo.addEventListener("click", () => bl3.onButtonClick());
 
 let buttonElemLift = document.querySelector(".btn-info-charakt-lift");
 let divChange4 = document.querySelector(".punkt-lift");
 let outputMoreLift = document.querySelector(".more-info-lift");
 let bl4 = new MyBlockClass(buttonElemLift, divChange4, outputMoreLift);
-let newFunctLift = function () {
-  buttonElemLift.addEventListener("click", bl4.onButtonClick());
-};
+buttonElemLift.addEventListener("click", () => bl4.onButtonClick());
+
 
 let buttonElemArea = document.querySelector(".btn-info-charakt-terr");
 let divChange5 = document.querySelector(".punkt-terr");
 let outputMoreArea = document.querySelector(".more-info-terr");
 let bl5 = new MyBlockClass(buttonElemArea, divChange5, outputMoreArea);
-let newFunctArea = function () {
-  buttonElemArea.addEventListener("click", bl5.onButtonClick());
-};
+buttonElemArea.addEventListener("click", () => bl5.onButtonClick());
 
 let buttonElemFenst = document.querySelector(".btn-info-charakt-fenst");
 let divChange6 = document.querySelector(".punkt-fenst");
 let outputMoreFenst = document.querySelector(".more-info-fenst");
 let bl6 = new MyBlockClass(buttonElemFenst, divChange6, outputMoreFenst);
-let newFunctFenst = function () {
-  buttonElemFenst.addEventListener("click", bl6.onButtonClick());
-};
+buttonElemFenst.addEventListener("click", () => bl6.onButtonClick());
+
 
 let buttonElemSignal = document.querySelector(".btn-info-charakt-signal");
 let divChange7 = document.querySelector(".punkt-signal");
 let outputMoreSignal = document.querySelector(".more-info-signal");
 let bl7 = new MyBlockClass(buttonElemSignal, divChange7, outputMoreSignal);
-let newFunctSignal = function () {
-  buttonElemSignal.addEventListener("click", bl7.onButtonClick());
-};
+buttonElemSignal.addEventListener("click", () => bl7.onButtonClick());
+
 
 let buttonElemKond = document.querySelector(".btn-info-charakt-kond");
 let divChange8 = document.querySelector(".punkt-kond");
 let outputMoreKond = document.querySelector(".more-info-kond");
 let bl8 = new MyBlockClass(buttonElemKond, divChange8, outputMoreKond);
-let newFunctKond = function () {
-  buttonElemKond.addEventListener("click", bl8.onButtonClick());
-};
+buttonElemKond.addEventListener("click", () => bl8.onButtonClick());
 
 let buttonElemVent = document.querySelector(".btn-info-charakt-vent");
 let divChange9 = document.querySelector(".punkt-vent");
 let outputMoreVent = document.querySelector(".more-info-vent");
 let bl9 = new MyBlockClass(buttonElemVent, divChange9, outputMoreVent);
-let newFunctVent = function () {
-  buttonElemVent.addEventListener("click", bl9.onButtonClick());
-};
+buttonElemVent.addEventListener("click", () => bl9.onButtonClick());
+
 
 
